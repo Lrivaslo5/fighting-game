78ec420aecde61cde0308150eae21abb53ade4e7
Luis Rivas Lopez <76708966+Lrivaslo5@users.noreply.github.com>
2022-05-02 13:53:05 -0400

    Finsihed attack mechanisms, adding health Ui

--- index.js
+++ index.js
@@ -122,6 +122,16 @@ const keys = { //
     }
 }
 
+// detect for collision between two rectangles, resuable for enemy and main player
+    function rectangularCollision({rectangle1, rectangle2}) {
+    return(
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height 
+    );
+}
+
 // Animation Loops, call for position and velocty updates
 function animate() {
     window.requestAnimationFrame(animate);
@@ -149,15 +159,25 @@ function animate() {
 
     // Collison detection module -- only when attack box is overlapping enemy sprite x and y axis considered
     if(
-        player.attackBox.position.x + player.attackBox.width >= enemy.position.x && 
-        player.attackBox.position.x <= enemy.position.x + enemy.width &&
-        player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
-        player.attackBox.position.y <= enemy.position.y + enemy.height &&
+        rectangularCollision({
+            rectangle1: player,
+            rectangle2: enemy
+        }) &&
         player.isAttacking
-    )   {
+    ){
         player.isAttacking = false;
         console.log('hit');   
-    }    
+    } 
+    if(
+        rectangularCollision({
+            rectangle1: enemy,
+            rectangle2: player
+        }) &&
+        enemy.isAttacking
+    ){
+    enemy.isAttacking = false;
+    console.log('enemy hit you');  
+    }
 }
 
 
@@ -201,7 +221,11 @@ window.addEventListener('keydown' , (event) => {
             
         case 'ArrowUp':
             enemy.velocity.y = -20;
-            break  
+            break
+
+        case 'ArrowDown':
+            enemy.isAttacking = true;
+            break 
     }
     console.log(event.key);
 })
