90329b39efed127ad7763cb187f4c5f7d04fe546
Luis Rivas Lopez <76708966+Lrivaslo5@users.noreply.github.com>
2022-05-01 23:39:33 -0400

    Added gravity and floor functionality

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-// Project Set Up
+// Project Set Up, creating a background canvas for game
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d') ;
 
-canvas.width = 1024;
+canvas.width = 1024; // Establishing canvas height and width (eventually want to be variable)
 canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height); 
 
-// Using OOP to ensure that avatars can interact and have different attributes -- Ex. Position, Gravity
+const gravity = 0.2
+
+// Using OOP to ensure that avatars can interact and have different attributes -- Ex. Position, velocity
 class Sprite{
     constructor({position, velocity}){ // position/velcoity are passed as an object to minimze importance of order and allows for only one to be passed if needed
         this.position  = position; // instance of object destructuing allows for cleaner syntax and same functionality
         this.velocity = velocity;
+        this.height = 150;
     }
 
     draw() {
         c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 50, 150);
+        c.fillRect(this.position.x, this.position.y, 50, this.height);
+    }
+
+    update() { //This function will be used in the animation section to create a new frame for each loop
+        this.draw();
+        this.position.y += this.velocity.y; 
+        if(this.position.y + this.height >= canvas.height) { // Adding a floor to stop velocity change
+            this.velocity.y = 0;
+        } else 
+        this.velocity.y += gravity // increase of Y-velocity allows for use to simulate gravity
     }
 }
 
@@ -42,16 +54,18 @@ const enemy = new Sprite({
     }
 })
 
-player.draw();
-enemy.draw();
+
 
 console.log(player);
 
-// Animation Loop
+// Animation Loop, calls for position and velocty update
 
 function animate() {
-    console.log('p');
     window.requestAnimationFrame(animate);
+    c.fillStyle = 'black' // e
+    c.fillRect(0, 0, canvas.width, canvas.height); // Clears canvas to create a new unique frame for animation
+    player.update();
+    enemy.update();
     
 }
 
